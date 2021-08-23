0d896509db9f48111b686e4b9a0421770dde64fb
Qifeng <qifeng.bai@csiro.au>
2021-08-24 08:44:12 +1000

    Grails 4 upgrade for Angular appliation

---
+++ grails-app/assets/javascripts/ala/ala-charts.js
@@ -0,0 +1,10 @@
+/*
+ * Replaces module charts from old resources plugin
+//= encoding UTF-8
+//= require bootstrap-toggle.min.js
+//= require bootstrap-multiselect.js
+//= require Chart.min.js
+//= require ALAChart.js
+//= require regionTimeControls.js
+//= require moment.min.js
+ */
---
+++ grails-app/taglib/au/org/ala/charts/ChartsTagLib.groovy
@@ -0,0 +1,49 @@
+package au.org.ala.charts
+
+import grails.converters.JSON
+import grails.util.Metadata
+import groovy.json.JsonSlurper
+
+/**
+ * Taglib for rendering charts
+ */
+class ChartsTagLib {
+
+    static namespace = "charts"
+
+    static chartsConfig = null
+
+    /**
+     * Render the specified charts
+     */
+    def biocache = { attrs ->
+
+        def jsonConfig = [
+            biocacheServiceUrl : attrs.biocacheServiceUrl,  // 'http://records-ws.als.scot'
+            biocacheWebappUrl : attrs.biocacheWebappUrl,    // 'http://records.als.scot'
+            query : attrs.q ?: '*:*',                       // 'lsid:\"${tc.taxonConcept.guid}\"'
+            queryContext : attrs.qc ?: '',                  // 'cl2:Scotland'
+            facetQueries : attrs.fq ?: [],
+            qualityProfile : attrs.qualityProfile ?: '',
+            charts : getChartConfig().biocache,
+            chartControls : attrs.chartControls ?: false,
+            chartControlsCallback: attrs.chartControlsCallback
+        ]
+
+        def chartVariableName = attrs.chartVariableName ?: 'chartConfig'
+
+        out << "var ${chartVariableName} = ${(jsonConfig as JSON).toString()};"
+        if (attrs?.autoLoad != 'false')
+            out << "var charts = ALA.BiocacheCharts('charts', chartConfig);"
+    }
+
+    private Object getChartConfig() {
+        if (chartsConfig == null) {
+            def appName = Metadata.current.'info.app.name'
+            def configPath = "/data/${appName}/config/charts.json"
+            def js = new JsonSlurper()
+            chartsConfig = js.parse(new FileReader(new File(configPath)))
+        }
+        chartsConfig
+    }
+}
